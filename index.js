1bb719343cd00f0bce891b0f5ccf3a3cd6bb744c
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-09-08 16:20:08 +0600

    set query on cart

--- index.js
+++ index.js
@@ -40,6 +40,15 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
 
     // cart collection
     app.post('/carts', async (req, res) => {
