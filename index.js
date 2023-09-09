0d1239dff1c899807ec3aa28ed93c81ab69c8b39
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-09-10 02:03:22 +0600

    added admin api and generate jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 //  middleware
@@ -26,15 +27,47 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("Bistrodb").collection("users");
     const menuCollection = client.db("Bistrodb").collection("menu");
     const reviewCollection = client.db("Bistrodb").collection("reviews");
     const cartCollection = client.db("Bistrodb").collection("carts");
 
+    // user related api
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'User already exists'})
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+// Admin related api
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+
+    // Menu related api
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
-
+    // review related api
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
