b88f8af83826daae2c8a060b9ea73eee3c9b851c
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-09-09 16:50:09 +0600

    added cart items delete to server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //  database connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blgp36b.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -55,6 +55,14 @@ async function run() {
       const cart = req.body;
       // console.log(item);
       const result = await cartCollection.insertOne(cart);
+      res.send(result);
+    })
+
+    // delete cart item
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
