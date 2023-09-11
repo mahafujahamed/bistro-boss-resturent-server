0b2a084c7aa9535f2acd711f6fde015294996bd8
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-09-11 06:39:16 +0600

    adds payment process

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 //  middleware
 app.use(cors());
 app.use(express.json());
+
+// verify token
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({error: true, message: 'Unauthorized access'});
+  }
+const token = authorization.split(' ')[1];
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  if(err){
+    return res.status(403).send({error: true, message: 'Forbidden access'});
+  }
+  req.decoded = decoded;
+  next();
+})
+}
 
 //  database connection
 
@@ -39,8 +56,18 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'});
       res.send({token});
     })
+// Warning: use verifyJWT before verifyAdmin
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if(user?.role !== 'admin'){
+        return res.status(403).send({error: true, message: 'Forbidden access'});
+      }
+      next();
+    }
     // user related api
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -55,6 +82,20 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin'};
+      res.send(result);
+    })
+
 // Admin related api
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
@@ -74,16 +115,33 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
+
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    })
+
+    app.delete('/menu/:id', verifyJWT, verifyAdmin,  async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
     // review related api
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     })
 
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if(!email){
         res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'Forbidden access'});
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
@@ -105,6 +163,21 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+    // create payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price} = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+        // Verify your integration in this guide by including this parameter
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
